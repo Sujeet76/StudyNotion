722b69d639c9699323d5dba5fb2b5aad404c9562
Sujeet kumar <ksujeetkumar7678@gmail.com>
2023-09-20 00:04:53 +0530

    responsive layout from sendOtp page

--- client/src/Components/Common/GenericContainer.component.jsx
+++ client/src/Components/Common/GenericContainer.component.jsx
@@ -2,8 +2,8 @@ import { Link } from "react";
 
 const GenericContainerComponent = ({ heading, subheading, children }) => {
   return (
-    <div className="bg-richblack-900 w-11/12 lg:h-[calc(100vh-3.5rem)] h-[calc(100vh - 82px)] grid place-content-center p-8">
-      <div className="lg:max-w-[33rem] w-full p-8 ">
+    <div className="bg-richblack-900 w-11/12 lg:h-[calc(100vh-3.5rem)] h-[calc(100vh - 82px)] grid place-content-center lg:mt-0 mt-10" >
+      <div className="lg:max-w-[33rem] w-full lg:p-8 ">
         <h1 className="lg:text-4xl text-3xl text-richblack-5 font-semibold">
           {heading}
         </h1>
