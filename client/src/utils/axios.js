949ee33ae5f51cbeb1a0457b3d8d14247c27d4bb
sujeet kumar <ksujeetkumar7678@gmail.con>
2023-09-20 23:11:54 +0530

    bug tracking mail send

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://aggressive-calf-wig.cyclic.cloud/api/v1",
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://aggressive-calf-wig.cyclic.cloud/api/v1",
 });
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
